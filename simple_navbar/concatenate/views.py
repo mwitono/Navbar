c59d249eac5abbd87004dfb63c45a93639070ce3
mwitono <witono1@yahoo.com>
2020-04-17 21:34:46 +0700

    concate integrate

--- simple_navbar/concatenate/views.py
+++ simple_navbar/concatenate/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request) :
-    return render(request, 'concatenate.html')
+    context = {
+        'title' : 'CONCAT',
+        'heading' : 'CONCATENATE',
+        'subheading' : 'CONCATENATE is statiscal modeling projects as the extension of excellent business operational model to explore all possibilities of performance increase and efficiencies, find the right model and implement it in no time.',
+
+    }
+
+    return render(request, 'concatenate\index.html', context)
