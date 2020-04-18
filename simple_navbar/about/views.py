07919f9b5c9cc78905609740c2b6b918ab0ba2ef
mwitono <witono1@yahoo.com>
2020-04-18 12:11:56 +0700

    about & concate title

--- simple_navbar/about/views.py
+++ simple_navbar/about/views.py
@@ -9,4 +9,4 @@ def index(request) :
         'subheading' : 'We are the team of data analyst and application builder. 30 Years total experience in Financial Information Technology and Infrastructure',
 
     }
-    return render(request, 'about/index.html', context)
+    return render(request, 'about\index.html', context)
--- simple_navbar/project0420/urls.py
+++ simple_navbar/project0420/urls.py
@@ -2,17 +2,18 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 from . import views
-from about import views as aboutViews
-from data import views as dataViews
-from concatenate import views as concatenateViews
+#from about import views as aboutViews
+#from data import views as dataViews
+#from concatenate import views as concatenateViews
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+
     url(r'^about/', include('about.urls')),
     url(r'^data/', include('data.urls')),
     url(r'^concatenate/', include('concatenate.urls')),
     #url(r'^concatenate/$', concatenateViews.index),
     #url(r'^about/$', aboutViews.index),
     #url(r'^data/$', dataViews.index),
-    url(r'^$', views.index),
+    url(r'^$', views.index, name='index'),
 ]
