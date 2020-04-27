4c35643ad1cc83fde9ce5166804fdc26634faa9e
mwitono <witono1@yahoo.com>
2020-04-27 22:42:35 +0700

    new pandas for data apps

--- simple_navbar/data/views.py
+++ simple_navbar/data/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .forms import UploadFileForm
 from django.core.files.storage import FileSystemStorage
+import xlrd
 import pandas as pd
 import numpy as np
-#import xlsxwriter
+import xlsxwriter
 
 # Create your views here.
 def index(request) :
@@ -22,5 +23,7 @@ def index(request) :
         'subheading' : 'In this page, you will need to upload your data profile. Please ensure you have validated your data before uploaded',
         'nama_file' : file_name,
     }
+
+    d1 = pd.read_excel(r'data/media/Data_Final.xlsx')
 
     return render(request, 'data/index.html', context)
