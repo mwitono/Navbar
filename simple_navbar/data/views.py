bd1570786591b298f7059f91918ae6c72c1067e0
mwitono <witono1@yahoo.com>
2020-04-27 21:53:04 +0700

    data file upload input

--- simple_navbar/data/views.py
+++ simple_navbar/data/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from .forms import UploadFileForm
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def index(request) :
+    #file_upload = UploadFileForm()
+    file_name = 'NONE'
+
+    if request.method == "POST" :
+        uploaded_file = request.FILES['training']
+        file_name = uploaded_file.name 
+        fs = FileSystemStorage()
+        fs.save(uploaded_file.name, uploaded_file)
+
     context = {
         'title' : 'Data',
         'heading' : 'Data',
         'subheading' : 'In this page, you will need to upload your data profile. Please ensure you have validated your data before uploaded',
-
+        'nama_file' : file_name,
     }
 
-    return render(request, 'data\index.html', context)
+    return render(request, 'data/index.html', context)
