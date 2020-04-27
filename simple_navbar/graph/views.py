07a5d5ac567e127472eccb4320ece8e4da13590f
mwitono <witono1@yahoo.com>
2020-04-27 21:04:55 +0700

    graph modules and py environment

--- simple_navbar/graph/views.py
+++ simple_navbar/graph/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+import matplotlib.pyplot as plt
+import io
+import urllib, base64
+from pylab import *
 
-# Create your views here.
 def index(request) :
-    context = {
-        'title' : 'Graph',
-        'heading' : 'Graph',
-        'subheading' : 'Show your charts here',
+    #plt.plot(range(10))
 
-    }
+    t = arange(0.0, 2.0, 0.01)
+    s = sin(2*pi*t)
+    plt.plot(t, s, linewidth=1.0)
+ 
+    xlabel('time (s)')
+    ylabel('voltage (mV)')
+    title('About as simple as it gets, folks')
+    grid(True)
 
-    return render(request, 'graph\index.html', context)
+    fig = plt.gcf()
+    buf = io.BytesIO()
+    fig.savefig(buf,format='png')
+    buf.seek(0)
+    string = base64.b64encode(buf.read())
+    uri = urllib.parse.quote(string)
+    return render(request, 'graph/index.html', {'data':uri})
